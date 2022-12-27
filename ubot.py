6f3e9c9d50ebfe6f50ebe7d0a4a730c3b1af890b
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-27 11:22:35 +0700

    Update ubot.py

--- ubot.py
+++ ubot.py
@@ -10,3 +10,43 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gpytranslate import Translator
 from pyrogram import Client
 from pytgcalls import GroupCallFactory
+
+
+LOG_FILE_NAME = "logs.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+
+LOGS = logging.getLogger(__name__)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+
+ubot = (
+    Client(
+        name="ubot",
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+        session_string=config.STRING_SESSION,
+        plugins=dict(root="HydraUb/plugins"),
+    )
+
+
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+# Clone by hdiiofficial :v
