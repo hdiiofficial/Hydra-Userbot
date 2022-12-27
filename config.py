66f6b7196b6a23017329cb5e38acee57a81d6a65
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-27 11:29:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ load_dotenv("config.env")
 
 # connection
 API_ID = int(getenv("API_ID", "")) 
-API_HASH = getenv("API_HASH")
+API_HASH = getenv("API_HASH", "")
 STRING_SESSION = getenv("STRING_SESSION", "")
+TOKEN = getenv("TOKEN", "")
 # other vars
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split(", ")))
