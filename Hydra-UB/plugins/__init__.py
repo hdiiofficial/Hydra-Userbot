9cb0cbfba473012afca9637b8a426ea8064bb9c0
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-27 15:28:14 +0700

    Create __init__.py

---
+++ Hydra-UB/plugins/__init__.py
@@ -0,0 +1,25 @@
+import glob
+from os.path import basename, dirname, isfile
+
+from config import *
+from Hydra-UB import *
+from Hydra-UB.helpers import *
+from Hydra-UB.helpers.SQL import *
+from Hydra-UB.resources import *
+from Hydra-UB.utils import *
+
+
+def __list_all_modules():
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
