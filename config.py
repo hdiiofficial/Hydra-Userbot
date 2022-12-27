134b3a3891c9c69d4aa903b6e010f208a43c585d
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-27 10:47:06 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+# connection
+API_ID = int(getenv("API_ID", "")) 
+API_HASH = getenv("API_HASH")
+STRING_SESSION = getenv("STRING_SESSION", "")
+# other vars
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split(", ")))
