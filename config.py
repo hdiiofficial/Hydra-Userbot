5990fb3d65908b3d2a85fbbd3fa88e933651bc36
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-27 11:57:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ load_dotenv("config.env")
 # connection
 API_ID = int(getenv("API_ID", "")) 
 API_HASH = getenv("API_HASH", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
 TOKEN = getenv("TOKEN", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+
 # other vars
+DB_URL = getenv("DB_URL", "")
+UBOT_LOG = int(getenv("UBOT_LOG") or 0)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split(", ")))
