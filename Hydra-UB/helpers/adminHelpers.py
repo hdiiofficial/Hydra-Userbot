96c49234c035dcb9a512378f4d912a70f542ed1e
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2022-12-28 20:46:14 +0530

    Update adminHelpers.py

--- Hydra-UB/helpers/adminHelpers.py
+++ Hydra-UB/helpers/adminHelpers.py
@@ -4,7 +4,7 @@ from time import time
 from pyrogram import Client
 from pyrogram.types import Message
 
-from Hydra-UB.helpers.interval import IntervalHelper
+from HydraUB.helpers.interval import IntervalHelper
 
 
 async def CheckAdmin(client: Client, message: Message):
