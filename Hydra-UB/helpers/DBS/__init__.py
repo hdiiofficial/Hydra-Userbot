bc76fe31a5c323908837dffc4dae09bb044d805b
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2022-12-28 20:53:08 +0530

    Update __init__.py

--- Hydra-UB/helpers/DBS/__init__.py
+++ Hydra-UB/helpers/DBS/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from Hydra-UB import DB_URL, LOGGER
+from HydraUB import DB_URL, LOGGER
 
 SPAMBOT = "SPAMBOT"
 
